272941e3015cac76f6a1831624e72c40de3bfd49
CesarYamadaX <122991507+CesarYamadaX@users.noreply.github.com>
2025-02-16 18:09:39 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,18 +10,27 @@ function App() {
 
   return (
     <>
-      <h1 className="title">Login</h1>
+      <header className="header" style={{ textAlign: "center" }}>
+        <h1 className="company-title">Poncho Corp</h1>
+      </header>
+      <h2 className="title">Login</h2>
+
       <form onSubmit={handleLogin} className="login-form">
         <div className="login-row">
-          <label for="username-input">Username:</label>
-          <input id="username-input" type="text" placeholder="username" required/>
+          <label htmlFor="username-input">Username:</label>
+          <input id="username-input" type="text" placeholder="username" required />
         </div>
         <div className="login-row">
-          <label for="password-input">Password:</label>
-          <input id="password-input" type="password" required/>
+          <label htmlFor="password-input">Password:</label>
+          <input id="password-input" type="password" required />
         </div>
         <button className="login-btn">Login</button>
       </form>
+
+      <div className="extra-links" style={{ display: "flex", justifyContent: "space-between", marginTop: "10px", fontSize: "0.9em" }}>
+        <p style={{ cursor: "pointer", color: "#555", marginLeft: "10px" }}>Crear nuevo usuario</p>
+        <p style={{ cursor: "pointer", color: "#555", marginRight: "10px" }}>Recuperar contraseña</p>
+      </div>
     </>
   );
 }
